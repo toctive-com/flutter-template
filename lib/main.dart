3d447920e8cffd444d5bf6d2522041731bb02391
Ikyrillos <kyrillosmaherfekri@gmail.com>
2023-06-10 16:53:24 +0300

    add: screen util
    Changes to be committed:
    	modified:   lib/main.dart
    	modified:   pubspec.lock
    	modified:   pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,20 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: const Scaffold(
         body: Center(
           child: Text('Hello World!'),
         ),
       ),
+      builder: (context, child) {
+        return MaterialApp(
+          home: child,
+        );
+      },
     );
   }
 }
